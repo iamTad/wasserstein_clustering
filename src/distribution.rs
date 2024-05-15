72e3657cbfe19d038870edc3c943f6b259126cc3
iamTad <6754728+iamTad@users.noreply.github.com>
2024-05-15 00:08:35 -0700

    created cdf generator

---
+++ src/distribution.rs
@@ -0,0 +1,13 @@
+pub struct EmpiricalDistribution(Vec<f64>)
+
+impl EmpiricalDistribution {
+    fn get_cdf(&self) -> Vec<f64>{
+        let mut cdf: Vec<f64> = Vec::new();
+        let norm_factor: f64 = self.0.iter().sum(); // how much to normalize empirical values
+        let mut cdf_point: f64 = 0; // keep track of previous value; to be incremented like a cdf
+        self.0
+            .iter()
+            .map(|value| {cdf_point += value; cdf_point/norm_factor})
+            .collect()
+    }
+}
