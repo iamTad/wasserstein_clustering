6049927ddd9652be6a93fec5ddd8431e88fdb425
iamTad <6754728+iamTad@users.noreply.github.com>
2024-05-19 20:40:58 -0700

    correctly use distribution module in main and added non-functional distance calculator

--- src/distribution.rs
+++ src/distribution.rs
@@ -1,8 +1,9 @@
-pub struct EmpiricalDistribution(Vec<f64>);
+#[derive(Debug)]
+pub struct EmpiricalDistribution(pub Vec<f64>);
 
 impl EmpiricalDistribution {
-    fn get_cdf(&self) -> Vec<f64>{
-        let mut cdf: Vec<f64> = Vec::new();
+    pub fn get_cdf(&self) -> Vec<f64>{
+        /// Takes the empirical distribution and creates a CDF
         let norm_factor: f64 = self.0.iter().sum(); // how much to normalize empirical values
         let mut cdf_point: f64 = 0.0; // keep track of previous value; to be incremented like a cdf
         self.0
@@ -10,4 +11,14 @@ impl EmpiricalDistribution {
             .map(|value| {cdf_point += value; cdf_point/norm_factor})
             .collect()
     }
+}
+
+
+pub fn calc_wasserstein_distance_1d(cdf_a: Vec<f64>, cdf_b: Vec<f64>) {
+    let norm_distance: u32 = if cdf_a.len() == cdf_b.len() { // the length of vectors must be the same
+        cdf_a.len()
+    } else {
+        1 // THIS IS JUST A PLACEHOLDER
+    }
+    let distance: f64 = 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use wasserstein_clustering::distribution;
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1.1, 2.2, 3.3];
+    let distro = distribution::EmpiricalDistribution(v);
+    let cdf = distro.get_cdf();
+    println!("{:?}", cdf);
 }
